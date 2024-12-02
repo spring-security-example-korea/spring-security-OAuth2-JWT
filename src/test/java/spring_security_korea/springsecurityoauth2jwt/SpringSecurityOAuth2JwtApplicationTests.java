11d78a8699e153bea4c1798892d9c6887dbd93ab
popeye0618 <popeye0618@gmail.com>
2024-12-02 19:41:01 +0900

    init

---
+++ src/test/java/spring_security_korea/springsecurityoauth2jwt/SpringSecurityOAuth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_security_korea.springsecurityoauth2jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuth2JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
