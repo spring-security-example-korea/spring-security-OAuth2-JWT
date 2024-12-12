f27db55abb7c696f121647ba97a82bd1602c5f0a
1223v <1223v@naver.com>
2024-12-13 03:53:42 +0900

    feat: MemberAuth 서비스 조회 기능 추가

---
+++ src/main/java/spring_security_korea/springsecurityoauth2jwt/member/service/MemberAuthService.java
@@ -0,0 +1,22 @@
+package spring_security_korea.springsecurityoauth2jwt.member.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import spring_security_korea.springsecurityoauth2jwt.member.domain.Member;
+import spring_security_korea.springsecurityoauth2jwt.member.repository.MemberRepository;
+import spring_security_korea.springsecurityoauth2jwt.security.jwt.repository.RefreshTokenRepository;
+
+@Service
+@RequiredArgsConstructor
+public class MemberAuthService {
+
+	private MemberRepository memberRepository;
+	private RefreshTokenRepository refreshTokenRepository;
+
+	public Member getMemberBySocialId(String socialId){
+		return memberRepository.findBySocialId(socialId);
+	}
+
+
+}
