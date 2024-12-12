dd9fa6dc0244281a9abc0fe57ebf4207d089c07a
popeye0618 <popeye0618@gmail.com>
2024-12-12 22:31:31 +0900

    feat: OAuth2ProviderUser 추가
    OAuth2Provider를 일부 구현한 추상 클래스

---
+++ src/main/java/spring_security_korea/springsecurityoauth2jwt/security/oauth2/OAuth2ProviderUser.java
@@ -0,0 +1,43 @@
+package spring_security_korea.springsecurityoauth2jwt.security.oauth2;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import lombok.Data;
+
+@Data
+public abstract class OAuth2ProviderUser implements ProviderUser {
+
+	private Map<String, Object> attributes;
+	private OAuth2User oAuth2User;
+	private ClientRegistration clientRegistration;
+
+	public OAuth2ProviderUser(Map<String, Object> attributes, OAuth2User oAuth2User,
+		ClientRegistration clientRegistration) {
+		this.attributes = attributes;
+		this.oAuth2User = oAuth2User;
+		this.clientRegistration = clientRegistration;
+	}
+
+	@Override
+	public String getEmail() {
+		return (String)attributes.get("email");
+	}
+
+	@Override
+	public String getProvider() {
+		return clientRegistration.getRegistrationId();
+	}
+
+	@Override
+	public List<? extends GrantedAuthority> getAuthorities() {
+		return oAuth2User.getAuthorities().stream().map(authority ->
+			new SimpleGrantedAuthority(authority.getAuthority())).collect(Collectors.toList());
+	}
+}
