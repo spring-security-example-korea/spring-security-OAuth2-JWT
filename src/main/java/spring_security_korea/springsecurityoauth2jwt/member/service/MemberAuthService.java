5b69ce434d548af8055bd75161d5bf1da24e5a64
1223v <1223v@naver.com>
2024-12-15 21:37:04 +0900

    chore: 자잘한 수정

--- src/main/java/spring_security_korea/springsecurityoauth2jwt/member/service/MemberAuthService.java
+++ src/main/java/spring_security_korea/springsecurityoauth2jwt/member/service/MemberAuthService.java
@@ -3,24 +3,26 @@ package spring_security_korea.springsecurityoauth2jwt.member.service;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import spring_security_korea.springsecurityoauth2jwt.exception.OAuth2AuthenticationProcessingException;
 import spring_security_korea.springsecurityoauth2jwt.member.domain.Member;
 import spring_security_korea.springsecurityoauth2jwt.member.repository.MemberRepository;
-import spring_security_korea.springsecurityoauth2jwt.security.jwt.repository.RefreshTokenRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MemberAuthService {
 
 	private MemberRepository memberRepository;
-	private RefreshTokenRepository refreshTokenRepository;
 
-	public Member getMemberBySocialId(String socialId){
-		return memberRepository.findBySocialId(socialId);
+	public Member getMemberBySocialId(String socialId) {
+		return memberRepository.findBySocialId(socialId).orElseThrow(
+			() -> new OAuth2AuthenticationProcessingException("유저를 찾을 수 없습니다.")
+		);
 	}
 
-	public Member getMemberByEmail(String email){
-		return memberRepository.findByEmail(email);
+	public Member getMemberByEmail(String email) {
+		return memberRepository.findByEmail(email).orElseThrow(
+			() -> new OAuth2AuthenticationProcessingException("유저를 찾을 수 없습니다.")
+		);
 	}
-
 
 }
